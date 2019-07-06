366199043552cbb20a25644d3172db924907daeb
Rui Esteves <ruicarest@gmail.com>
2019-07-06 12:39:06 +0100

    added Asteroids

--- src/Asteroid.js
+++ src/Asteroid.js
@@ -1,4 +1,4 @@
-import {randomNumBetween} from './utils';
+import {asteroidVertices, randomNumBetween} from './utils';
 
 export default class Asteroid {
     constructor(args) {
@@ -6,7 +6,14 @@ export default class Asteroid {
         this.velocity = {
             x: randomNumBetween(-1.5, 1.5),
             y: randomNumBetween(-1.5, 1.5),
-        }
+        };
+        this.vertices = asteroidVertices(8, args.size);
+        this.rotation = 0;
+        this.rotationSpeed = randomNumBetween(-1, 1)
+        this.radius = args.size;
+        this.score = (80/this.radius)*5;
+        this.create = args.create;
+        this.addScore = args.addScore;
 
         console.log("New asteroid created");
     }
@@ -16,6 +23,39 @@ export default class Asteroid {
     }
 
     render(state) {
+    // Move
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
 
+    // Rotation
+    this.rotation += this.rotationSpeed;
+    if (this.rotation >= 360) {
+      this.rotation -= 360;
     }
+    if (this.rotation < 0) {
+      this.rotation += 360;
+    }
+
+    // Screen edges
+    if(this.position.x > state.screen.width + this.radius) this.position.x = -this.radius;
+    else if(this.position.x < -this.radius) this.position.x = state.screen.width + this.radius;
+    if(this.position.y > state.screen.height + this.radius) this.position.y = -this.radius;
+    else if(this.position.y < -this.radius) this.position.y = state.screen.height + this.radius;
+
+    // Draw
+    const context = state.context;
+    context.save();
+    context.translate(this.position.x, this.position.y);
+    context.rotate(this.rotation * Math.PI / 180);
+    context.strokeStyle = '#FFF';
+    context.lineWidth = 2;
+    context.beginPath();
+    context.moveTo(0, -this.radius);
+    for (let i = 1; i < this.vertices.length; i++) {
+      context.lineTo(this.vertices[i].x, this.vertices[i].y);
+    }
+    context.closePath();
+    context.stroke();
+    context.restore();
+  }
 }
--- src/Swarm.js
+++ src/Swarm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Asteroid from './Asteroid';
+import { randomNumBetweenExcluding } from './utils';
 
 const KEY = {
     LEFT:  37,
@@ -29,7 +30,10 @@ export class Swarm extends Component {
                 width: window.innerWidth,
                 height: window.innerHeight,
                 ratio: window.devicePixelRatio || 1,
-            }
+            },
+            currentScore: 0,
+            inGame: false,
+            asteroidCount: 2,
         }
 
         this.ship = [];
@@ -67,15 +71,84 @@ export class Swarm extends Component {
 
         const context = this.refs.canvas.getContext('2d');
         this.setState({ context: context });
-        //this.startGame();
+        this.startGame();
         requestAnimationFrame(() => {this.update()});
 
         console.log("Rui - Mounted!");
 
     }
 
-    update() {
+    startGame() {
+        this.setState({
+        inGame: true,
+        currentScore: 0,
+        });
+    
+        // Make asteroids
+        this.asteroids = [];
+        this.generateAsteroids(this.state.asteroidCount)
+    }
 
+    generateAsteroids(howMany){
+        let asteroids = [];
+        let ship = this.ship[0];
+        for (let i = 0; i < howMany; i++) {
+          let asteroid = new Asteroid({
+            size: 80,
+            position: {
+              x: randomNumBetweenExcluding(0, this.state.screen.width, 0, 10),
+              y: randomNumBetweenExcluding(0, this.state.screen.height, 0, 10),
+            },
+            create: this.createObject.bind(this),
+            addScore: this.addScore.bind(this)
+          });
+          this.createObject(asteroid, 'asteroids');
+        }
+      }
+
+      createObject(item, group){
+        this[group].push(item);
+      }
+
+      updateObjects(items, group){
+        let index = 0;
+        for (let item of items) {
+          if (item.delete) {
+            this[group].splice(index, 1);
+          }else{
+            items[index].render(this.state);
+          }
+          index++;
+        }
+      }
+
+      addScore(points){
+        if(this.state.inGame){
+          this.setState({
+            currentScore: this.state.currentScore + points,
+          });
+        }
+      }
+
+    update() {
+        const context = this.state.context;
+    
+        context.save();
+        context.scale(this.state.screen.ratio, this.state.screen.ratio);
+    
+        // Motion trail
+        context.fillStyle = '#000';
+        context.globalAlpha = 0.4;
+        context.fillRect(0, 0, this.state.screen.width, this.state.screen.height);
+        context.globalAlpha = 1;
+       
+        // Remove or render
+        this.updateObjects(this.asteroids, 'asteroids')
+    
+        context.restore();
+    
+        // Next frame
+        requestAnimationFrame(() => {this.update()});
     }
 
     render() {
