d63ab23cd22693fd171b2685667b09772d483cec
Rui Esteves <ruicarest@gmail.com>
2019-07-08 23:58:27 +0100

    revert position init

--- src/Ship.js
+++ src/Ship.js
@@ -2,16 +2,17 @@ import { rotatePoint, randomNumBetween } from './utils';
 
 export default class Ship {
     constructor(args) {
-        this.velocity = {
-          x: 0,
-          y: 0
-        }
-        this.rotation = 0;
-        this.rotationSpeed = 6;
-        this.speed = 0.15;
-        this.inertia = 0.99;
-        this.radius = 20;
-        this.updateVelocity = args.updateVelocity;
+      this.position = args.position;
+      this.velocity = {
+        x: 0,
+        y: 0
+      }
+      this.rotation = 0;
+      this.rotationSpeed = 6;
+      this.speed = 0.15;
+      this.inertia = 0.99;
+      this.radius = 20;
+      this.updateVelocity = args.updateVelocity;
     }
 
     destroy() {
