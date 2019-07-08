62c6d97480fcf25da1746fa0d7bc24042c360a0e
Rui Esteves <ruicarest@gmail.com>
2019-07-08 23:12:23 +0100

    create ship and asteroids on minimap

--- src/Minimap.js
+++ src/Minimap.js
@@ -18,20 +18,29 @@ export class Minimap extends Component {
     //draw asteroids on minimap
     Asteroids.forEach(asteroid => {
       //TODO: correct minimap scale
-       this.createCircle(asteroid.position.x/10, asteroid.position.y/10);
+       this.createCircle(
+        (asteroid.position.x)/10,
+        (asteroid.position.y)/10, 
+        '#b00c1f');
+    });
+
+    //draw the ship on minimap
+    Ship.forEach(ship => {
+      //TODO: correct minimap scale
+        this.createCircle(ship.position.x/10, ship.position.y/10, '#424bf5');
     });
 
 
   }
 
   //draw circle on minimap
-  createCircle = (x, y) => {
+  createCircle = (x, y, colour) => {
     this.state.ctx.save();
-    this.state.ctx.strokeStyle = '#ffffff';
-    this.state.ctx.fillStyle = '#000000';
-    this.state.ctx.lineWidth = 2;
+    this.state.ctx.strokeStyle = colour;
+    this.state.ctx.fillStyle = colour;
+    this.state.ctx.lineWidth = 1;
     this.state.ctx.beginPath();
-    this.state.ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    this.state.ctx.arc(x, y, 2, 0, 2 * Math.PI);
     this.state.ctx.fill();
     this.state.ctx.stroke();
     this.state.ctx.restore();
