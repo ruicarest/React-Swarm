bdbe3525d7d4261afbe868c7790070dbc4627b1f
Rui Esteves <ruicarest@gmail.com>
2019-07-08 22:38:28 +0100

    draw asteroids on minimap

--- src/Minimap.js
+++ src/Minimap.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 
 export class Minimap extends Component {
-    constructor() {
-        super();
-        
-        this.state = {
-        }
 
-    }
+  state = {};
 
-    componentDidMount() {
+  componentDidMount = () => {
+    //get minimap canvas context
+    this.state.ctx = this.refs.minimap.getContext('2d');
+  }
 
-    }
+  componentDidUpdate = () => {
+    const { Asteroids , Ship } = this.props;
 
-    update() {
+    //clear canvas
+    this.state.ctx.clearRect(0, 0, this.state.ctx.canvas.width, this.state.ctx.canvas.height);
 
-    }
+    //draw asteroids on minimap
+    Asteroids.forEach(asteroid => {
+      //TODO: correct minimap scale
+       this.createCircle(asteroid.position.x/10, asteroid.position.y/10);
+    });
 
-    render() {
-        return (
-          <div>
-            <canvas className="minimap" ref="minimap"/>
-          </div>
-        );
-    }
+
+  }
+
+  //draw circle on minimap
+  createCircle = (x, y) => {
+    this.state.ctx.save();
+    this.state.ctx.strokeStyle = '#ffffff';
+    this.state.ctx.fillStyle = '#000000';
+    this.state.ctx.lineWidth = 2;
+    this.state.ctx.beginPath();
+    this.state.ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    this.state.ctx.fill();
+    this.state.ctx.stroke();
+    this.state.ctx.restore();
+  }
+
+  update = () => {
+
+  }
+
+  render = () => {
+      return (
+        <div>
+          <canvas className="minimap" ref="minimap"/>
+        </div>
+      );
+  }
 
 }
