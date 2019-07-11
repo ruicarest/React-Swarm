49740cfe7ce491f4d7b310764255437e41f85176
Rui Esteves <ruicarest@gmail.com>
2019-07-11 23:19:44 +0100

    collisions

--- src/Swarm.js
+++ src/Swarm.js
@@ -171,6 +171,9 @@ export class Swarm extends Component {
       context.fillRect(0, 0, this.state.screen.width, this.state.screen.height);
       context.globalAlpha = 1;
       
+      // Check for colisions
+      this.checkCollisionsWith(this.bullets, this.asteroids);
+
       // Remove or render
       this.updateObjects(this.asteroids, 'asteroids');
       this.updateObjects(this.ship, 'ship');
@@ -181,6 +184,33 @@ export class Swarm extends Component {
       // Next frame
       requestAnimationFrame(() => {this.update()});
     }
+
+    checkCollisionsWith(items1, items2) {
+      var a = items1.length - 1;
+      var b;
+      for(a; a > -1; --a){
+        b = items2.length - 1;
+        for(b; b > -1; --b){
+          var item1 = items1[a];
+          var item2 = items2[b];
+          if(this.checkCollision(item1, item2)){
+            item1.destroy();
+            item2.destroy();
+          }
+        }
+      }
+    }
+
+    checkCollision(obj1, obj2){
+      var vx = obj1.position.x - obj2.position.x;
+      var vy = obj1.position.y - obj2.position.y;
+      var length = Math.sqrt(vx * vx + vy * vy);
+      if(length < obj1.radius + obj2.radius){
+        return true;
+      }
+      return false;
+    }
+
 
     render() {
         return (
