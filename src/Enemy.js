5e4f0962eb125666e834c932021555a7c997e3a4
Rui Esteves <ruicarest@gmail.com>
2019-08-07 16:32:54 +0100

    fix on enemy damage

--- src/Enemy.js
+++ src/Enemy.js
@@ -154,7 +154,7 @@ export default class Enemy {
             if(Date.now() - this.T_lastShot > 1000){
                 const bullet = new Bullet({
                   ship: this, 
-                  amage: 10,
+                  damage: 10,
                   create:this.create.bind(this)
                 });
                 this.create(bullet, 'enemyBullets');
