4b0cabe75fa2e8e51abfa4212cf99542f2a327e2
ruicarest <ruicarest@gmail.com>
2020-03-19 16:11:45 +0000

    InputManager

--- src/Components/Swarm/Swarm.js
+++ src/Components/Swarm/Swarm.js
@@ -15,6 +15,7 @@ import { VirtualJoystick } from "../VirtualJoyStick/VirtualJoystick";
 import ammoTypes from "../../Configs/ammoTypes.json";
 import "./Swarm.css";
 import MessageBox from "../MessageBox/MessageBox";
+import InputManager from "../../Managers/InputManager";
 
 import _ from "lodash";
 
@@ -22,27 +23,16 @@ const CFGS = {
   TILE_SIZE: 64
 };
 
-const KEY = {
-  LEFT: 37,
-  RIGHT: 39,
-  UP: 38,
-  A: 65,
-  D: 68,
-  W: 87,
-  SPACE: 32,
-  Q: 81
-};
-
 export default class Swarm extends Component {
   constructor() {
     super();
 
     this.currentMap = 0;
-
     this.MAP = maps[this.currentMap];
 
     this.isMobileBrowser = checkIfRunningOnMobileDevice();
 
+    this.InputManager = new InputManager();
     this.bullets = [];
     this.enemyBullets = [];
     this.ship = [];
@@ -51,177 +41,10 @@ export default class Swarm extends Component {
     this.energy = [];
     this.EZT = [];
     this.enemies = [];
-
-    this.handleJoystick = this.handleJoystick.bind(this);
-  }
-
-  handleResize(value, e) {
-    let offset = {
-      x: this.refs.gameWindow.clientWidth / 2 - this.props.screen.width / 2,
-      y: this.refs.gameWindow.clientHeight / 2 - this.props.screen.height / 2
-    };
-
-    this.updateObjectsPosition(offset);
-
-    this.props.updateState({
-      screen: {
-        width: this.refs.gameWindow.clientWidth,
-        height: this.refs.gameWindow.clientHeight,
-        ratio:
-          this.refs.gameWindow.clientWidth /
-            this.refs.gameWindow.clientHeight || 1
-      }
-    });
-  }
-
-  handleKeys(value, e) {
-    let keys = this.props.keys;
-    if (e.keyCode === KEY.LEFT || e.keyCode === KEY.A) keys.left = value;
-    if (e.keyCode === KEY.RIGHT || e.keyCode === KEY.D) keys.right = value;
-    if (e.keyCode === KEY.UP || e.keyCode === KEY.W) keys.up = value;
-    if (e.keyCode === KEY.SPACE) keys.space = value;
-    if (e.keyCode === KEY.Q) keys.mine = value;
-
-    this.props.updateState({ keys: keys });
-  }
-
-  handleJoystick(padAngle) {
-    this.props.updateState({
-      joypad: {
-        ...this.props.joypad,
-        angle: padAngle
-      }
-    });
-  }
-
-  loadEventListeners() {
-    //handle window resize
-    window.addEventListener(
-      "resize",
-      _.throttle(this.handleResize.bind(this, false), 100)
-    );
-
-    if (!this.isMobileBrowser) {
-      //handle key events
-      window.addEventListener("keyup", this.handleKeys.bind(this, false));
-      window.addEventListener("keydown", this.handleKeys.bind(this, true));
-
-      //handle mouse events
-      this.refs.gameWindow.addEventListener(
-        "mousemove",
-        function(e) {
-          this.props.updateState({
-            mouse: {
-              active: true,
-              position: {
-                x: e.clientX,
-                y: e.clientY
-              }
-            }
-          });
-        }.bind(this),
-        false
-      );
-    } else {
-      //handle touch events
-      this.refs.gameWindow.addEventListener(
-        "touchmove",
-        function(e) {
-          if (this.props.screen.width > e.touches[0].clientX * 2) {
-            this.props.updateState({
-              joypad: {
-                ...this.props.joypad,
-                moving: true,
-                stickPosition: {
-                  x: e.touches[0].clientX,
-                  y: e.touches[0].clientY
-                }
-              }
-            });
-          }
-        }.bind(this),
-        false
-      );
-      this.refs.gameWindow.addEventListener(
-        "touchstart",
-        function(e) {
-          const leftClick =
-            this.props.screen.width >
-            e.touches[e.changedTouches[0].identifier].clientX * 2;
-
-          if (leftClick) {
-            this.props.updateState({
-              joypad: {
-                ...this.props.joypad,
-                on: true,
-                basePosition: {
-                  x: e.touches[e.changedTouches[0].identifier].clientX,
-                  y: e.touches[e.changedTouches[0].identifier].clientY
-                },
-                stickPosition: {
-                  x: e.touches[e.changedTouches[0].identifier].clientX,
-                  y: e.touches[e.changedTouches[0].identifier].clientY
-                }
-              }
-            });
-          } else {
-            this.props.updateState({
-              joypad: {
-                ...this.props.joypad,
-                stickClickPosition: {
-                  x: e.touches[0].clientX,
-                  y: e.touches[0].clientY,
-                  on: true
-                }
-              }
-            });
-          }
-        }.bind(this),
-        false
-      );
-      this.refs.gameWindow.addEventListener(
-        "touchend",
-        function(e) {
-          const leftClick =
-            this.props.screen.width > e.changedTouches[0].clientX * 2;
-
-          /*         const rightClick =
-              this.props.screen.width < e.changedTouches[0].clientX * 2; */
-
-          if (leftClick) {
-            this.props.updateState({
-              joypad: {
-                ...this.props.joypad,
-                on: false,
-                moving: false,
-                basePosition: {
-                  x: 0,
-                  y: 0
-                }
-              }
-            });
-          } else {
-            this.props.updateState({
-              joypad: {
-                ...this.props.joypad,
-                stickClickPosition: {
-                  x: 0,
-                  y: 0,
-                  on: false
-                }
-              }
-            });
-          }
-        }.bind(this),
-        false
-      );
-    }
   }
 
   componentDidMount() {
-    //initiate handlers
-    this.loadEventListeners();
-
+    this.InputManager.init.call(this);
     this.props.updateGroup("game", {
       context: this.refs.gameWindow.getContext("2d")
     });
---
+++ src/Managers/InputManager.js
@@ -0,0 +1,203 @@
+import _ from "lodash";
+
+const KEY = {
+  LEFT: 37,
+  RIGHT: 39,
+  UP: 38,
+  A: 65,
+  D: 68,
+  W: 87,
+  SPACE: 32,
+  Q: 81
+};
+
+export default function InputManager() {
+  function init() {
+    //load Listeners;
+    initResize.call(this);
+
+    if (!this.isMobileBrowser) {
+      initDesktop.call(this);
+    } else {
+      initMobile.call(this);
+    }
+  }
+
+  function handleResize() {
+    let offset = {
+      x: this.refs.gameWindow.clientWidth / 2 - this.props.screen.width / 2,
+      y: this.refs.gameWindow.clientHeight / 2 - this.props.screen.height / 2
+    };
+
+    this.updateObjectsPosition(offset);
+
+    this.props.updateState({
+      screen: {
+        width: this.refs.gameWindow.clientWidth,
+        height: this.refs.gameWindow.clientHeight,
+        ratio:
+          this.refs.gameWindow.clientWidth /
+            this.refs.gameWindow.clientHeight || 1
+      }
+    });
+  }
+
+  function handleKeys(value, e) {
+    let keys = this.props.keys;
+    if (e.keyCode === KEY.LEFT || e.keyCode === KEY.A) keys.left = value;
+    if (e.keyCode === KEY.RIGHT || e.keyCode === KEY.D) keys.right = value;
+    if (e.keyCode === KEY.UP || e.keyCode === KEY.W) keys.up = value;
+    if (e.keyCode === KEY.SPACE) keys.space = value;
+    if (e.keyCode === KEY.Q) keys.mine = value;
+
+    this.props.updateState({ keys: keys });
+  }
+
+  function handleJoystick(padAngle) {
+    this.props.updateState({
+      joypad: {
+        ...this.props.joypad,
+        angle: padAngle
+      }
+    });
+  }
+
+  function initResize() {
+    console.log("initResize");
+
+    //handle window resize
+    window.addEventListener(
+      "resize",
+      _.throttle(handleResize.bind(this, false), 100)
+    );
+  }
+
+  function initMobile() {
+    //handle touch events
+    this.refs.gameWindow.addEventListener(
+      "touchmove",
+      function(e) {
+        if (this.props.screen.width > e.touches[0].clientX * 2) {
+          this.props.updateState({
+            joypad: {
+              ...this.props.joypad,
+              moving: true,
+              stickPosition: {
+                x: e.touches[0].clientX,
+                y: e.touches[0].clientY
+              }
+            }
+          });
+        }
+      }.bind(this),
+      false
+    );
+    this.refs.gameWindow.addEventListener(
+      "touchstart",
+      function(e) {
+        const leftClick =
+          this.props.screen.width >
+          e.touches[e.changedTouches[0].identifier].clientX * 2;
+
+        if (leftClick) {
+          this.props.updateState({
+            joypad: {
+              ...this.props.joypad,
+              on: true,
+              basePosition: {
+                x: e.touches[e.changedTouches[0].identifier].clientX,
+                y: e.touches[e.changedTouches[0].identifier].clientY
+              },
+              stickPosition: {
+                x: e.touches[e.changedTouches[0].identifier].clientX,
+                y: e.touches[e.changedTouches[0].identifier].clientY
+              }
+            }
+          });
+        } else {
+          this.props.updateState({
+            joypad: {
+              ...this.props.joypad,
+              stickClickPosition: {
+                x: e.touches[0].clientX,
+                y: e.touches[0].clientY,
+                on: true
+              }
+            }
+          });
+        }
+      }.bind(this),
+      false
+    );
+    this.refs.gameWindow.addEventListener(
+      "touchend",
+      function(e) {
+        const leftClick =
+          this.props.screen.width > e.changedTouches[0].clientX * 2;
+
+        /*         const rightClick =
+                this.props.screen.width < e.changedTouches[0].clientX * 2; */
+
+        if (leftClick) {
+          this.props.updateState({
+            joypad: {
+              ...this.props.joypad,
+              on: false,
+              moving: false,
+              basePosition: {
+                x: 0,
+                y: 0
+              }
+            }
+          });
+        } else {
+          this.props.updateState({
+            joypad: {
+              ...this.props.joypad,
+              stickClickPosition: {
+                x: 0,
+                y: 0,
+                on: false
+              }
+            }
+          });
+        }
+      }.bind(this),
+      false
+    );
+  }
+
+  function initDesktop() {
+    //handle key events
+    window.addEventListener("keyup", handleKeys.bind(this, false));
+    window.addEventListener("keydown", handleKeys.bind(this, true));
+
+    //handle mouse events
+    this.refs.gameWindow.addEventListener(
+      "mousemove",
+      function(e) {
+        this.props.updateState({
+          mouse: {
+            active: true,
+            position: {
+              x: e.clientX,
+              y: e.clientY
+            }
+          }
+        });
+      }.bind(this),
+      false
+    );
+  }
+
+  return {
+    init: init,
+    handleResize: handleResize,
+    handleKeys: handleKeys,
+    handleJoystick: handleJoystick
+  };
+}
+
+export let teste = function() {
+  console.log(this);
+};
